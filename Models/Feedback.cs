8b530520b5632ee12c0ed2e746d963ffa7d177c8
duy3167 <106375074+duy3167@users.noreply.github.com>
2024-03-03 09:52:58 +0700

    feedv1

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,6 +1,28 @@
-﻿namespace EnterpriceWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EnterpriceWeb.Models
 {
     public class Feedback
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int fb_id {  get; set; }
+        [Required]
+        public String date {  get; set; }
+        [Required]
+        public int article_id {  get; set; }
+        [ForeignKey("article_id")]
+        public Article? Article { get; set; }
+        [Required]
+        public int us_id {  get; set; }
+        [ForeignKey("us_id")]
+        public User? user { get; set; }
+        [Required]
+        public String content {  get; set; }
+        
+
+
     }
 }
